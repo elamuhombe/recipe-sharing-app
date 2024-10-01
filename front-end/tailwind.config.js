45b99cfc629d66ab4c45de9870eaa37799d24e40
Elaine Muhombe <emuhombe@gmail.com>
2024-10-01 17:04:55 +0300

    Setup front end project files

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Add your React component file types here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
